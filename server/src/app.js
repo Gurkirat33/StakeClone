6d66108b4dd3bcf8ec7ec0254eac8e6b4c44b348
Gurkirat Singh <gurkiratsingh12a@gmail.com>
2024-07-29 14:44:54 +0530

    update

--- server/src/app.js
+++ server/src/app.js
@@ -4,7 +4,7 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors({ origin: process.env.CORS_ORIGIN }));
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
